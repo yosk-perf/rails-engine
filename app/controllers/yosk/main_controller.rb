4c36c6e480b59dc63ad6d455b8cf474c6579d786
Yosi Attias <yosy101@gmail.com>
2019-03-24 23:12:04 +0200

    Faster routes

--- app/controllers/yosk/main_controller.rb
+++ app/controllers/yosk/main_controller.rb
@@ -3,28 +3,34 @@ require_dependency 'yosk/application_controller'
 module Yosk
   class MainController < ApplicationController
     def routes
-      routes = Rails.application.routes.routes.map(&:defaults)
-
-      controllers = routes.map { |c| c[:controller] }.uniq.compact
-
-      controllers.map! do |controller|
-        controller = "#{controller.camelize}Controller".safe_constantize
-        next if controller.nil?
-
-        actions = controller.action_methods.to_a
-        file = controller.instance_method(actions.first.to_sym).source_location[0]
-
-        next unless file.include? Rails.root.join('app').to_s
-
-        {
-          controller: controller.name,
-          actions: actions
-        }
-      end
-
-      controllers.compact!
-
       render json: controllers
+    end
+
+    private 
+
+    def controllers
+      @@controllers ||= begin
+        routes = Rails.application.routes.routes.map(&:defaults)
+
+        controllers = routes.map { |c| c[:controller] }.uniq.compact
+
+        controllers.map! do |controller|
+          controller = "#{controller.camelize}Controller".safe_constantize
+          next if controller.nil?
+
+          actions = controller.action_methods.to_a
+          file = controller.instance_method(actions.first.to_sym).source_location[0]
+
+          next unless file.include? Rails.root.join('app').to_s
+
+          {
+            controller: controller.name,
+            actions: actions
+          }
+        end
+
+        controllers.compact!
+                        end
     end
   end
 end
