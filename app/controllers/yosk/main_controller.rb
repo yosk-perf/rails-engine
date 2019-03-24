4ba1b8bcf7073195387a3d21c527a07aa0e5a833
Yosi Attias <yosy101@gmail.com>
2019-03-24 13:34:43 +0200

    code cleanup

--- app/controllers/yosk/main_controller.rb
+++ app/controllers/yosk/main_controller.rb
@@ -1,12 +1,13 @@
-require_dependency "yosk/application_controller"
+require_dependency 'yosk/application_controller'
 
 module Yosk
   class MainController < ApplicationController
     def routes
-      routes = Rails.application.routes.routes.map { |x| x.defaults }
+      routes = Rails.application.routes.routes.map(&:defaults)
 
       routes.map! do |route|
         next if route[:controller].nil?
+
         {
           controller: "#{route[:controller].classify}Controller",
           action: route[:action]
@@ -17,7 +18,7 @@ module Yosk
       routes = routes.group_by { |r| r[:controller] }.map do |controller_name, controller_actions|
         {
           controller: controller_name,
-          actions:  controller_actions.map { |a| a[:action] }
+          actions: controller_actions.map { |a| a[:action] }
         }
       end
 
