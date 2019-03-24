9df85016a412630fcef40e591394a4868950a714
Yosi Attias <yosy101@gmail.com>
2019-03-24 12:29:26 +0200

    Exposing routes

--- app/controllers/yosk/main_controller.rb
+++ app/controllers/yosk/main_controller.rb
@@ -3,7 +3,25 @@ require_dependency "yosk/application_controller"
 module Yosk
   class MainController < ApplicationController
     def routes
-      render json: { success: true }
+      routes = Rails.application.routes.routes.map { |x| x.defaults }
+
+      routes.map! do |route|
+        next if route[:controller].nil?
+        {
+          controller: "#{route[:controller].classify}Controller",
+          action: route[:action]
+        }
+      end
+
+      routes.compact!
+      routes = routes.group_by { |r| r[:controller] }.map do |controller_name, controller_actions|
+        {
+          controller: controller_name,
+          actions:  controller_actions.map { |a| a[:action] }
+        }
+      end
+
+      render json: routes
     end
   end
 end
