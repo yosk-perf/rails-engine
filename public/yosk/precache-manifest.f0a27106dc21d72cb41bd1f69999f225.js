8b9fa77e9d14de7be1d41b94ee5fa322126c9079
Yosi Attias <yosy101@gmail.com>
2019-03-24 23:12:25 +0200

    addding public files

---
+++ public/yosk/precache-manifest.f0a27106dc21d72cb41bd1f69999f225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8c469cc9c0fcc822b27",
+    "url": "/yosk/static/js/runtime~main.5549d84a.js"
+  },
+  {
+    "revision": "297b77680207735f71c4",
+    "url": "/yosk/static/js/main.0ebb04c2.chunk.js"
+  },
+  {
+    "revision": "862c3899b2eac01a7fee",
+    "url": "/yosk/static/js/2.b9a7ca11.chunk.js"
+  },
+  {
+    "revision": "297b77680207735f71c4",
+    "url": "/yosk/static/css/main.dbf6ccd7.chunk.css"
+  },
+  {
+    "revision": "862c3899b2eac01a7fee",
+    "url": "/yosk/static/css/2.14837808.chunk.css"
+  },
+  {
+    "revision": "8db2afc3e0d66feea01f2f4435a3b208",
+    "url": "/yosk/index.html"
+  }
+];
