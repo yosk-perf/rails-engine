1821f4b10bd6a5518708d123263e5ee11d4ec038
Yosi Attias <yosy101@gmail.com>
2019-03-26 21:23:31 +0200

    new frontend - with tabs

--- public/yosk/service-worker.js
+++ public/yosk/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/yosk/precache-manifest.b94201ff8b079106032ac734ef98cfc9.js"
+  "/yosk/precache-manifest.224fcc269dd82c77d6d880b294186ea7.js"
 );
 
 workbox.clientsClaim();
