3e86ec8ca23773a3cd2ab7b97ea757a5309c0e4b
Yosi Attias <yosy101@gmail.com>
2019-03-30 22:12:57 +0300

    Handle static files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,36 @@
+class StaticResponder
+  attr_accessor :path
+
+  def initialize(path)
+    self.path = path
+    trigger_file_handler_initialization
+  end
+
+  def trigger_file_handler_initialization
+    file_handler
+  end
+
+  def call(env)
+    env['PATH_INFO'] = @file_handler.match?(path)
+    @file_handler.call(env)
+  end
+
+  def inspect
+    "static('#{path}')"
+  end
+
+  def file_handler
+    @file_handler ||= ::ActionDispatch::FileHandler.new(
+      Yosk::Engine.root.join('public').join('yosk').to_s
+    )
+  end
+end
+
 Yosk::Engine.routes.draw do
   get 'routes', to: 'main#routes'
+
+  get '/', to: StaticResponder.new('index.html')
+  get '/:id', to: StaticResponder.new('index.html')
 
   resources :execution, only: [:create] do
     member do
